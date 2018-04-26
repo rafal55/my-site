4cc39dbbaf11e0f05b5b69930e905587aafacf1a
Rafal <rafal.baranowski5@gmail.com>
2018-04-27 00:13:47 +0200

    Start page url modified

--- web_page/urls.py
+++ web_page/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('start_page', views.start_page, name='start_page'),
+    path('', views.start_page, name='start_page'),
     path('home_page', views.home_page, name='home_page'),
     path('your_name', views.your_name, name='home_page'),
     path('contact', views.contact, name='home_page'),
